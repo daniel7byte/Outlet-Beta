fbc26fc8a09a618e0bb2ee36d93e34e9d6a2fea8
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2016-10-15 12:15:43 -0500

    2 Commit

--- listado.php
+++ listado.php
@@ -202,63 +202,47 @@
 <body>
   <?php include_once('include/navbar.php'); ?>
   
-  <div class="row navOptionsRow">
-    
-    <div class="col-md-12 navOptionsCol">
+
+  <div class="container-fluid" style="margin-top: 60px">
+    <div class="row">
       
-      <!-- <select class="selectpicker" id="">
-        <option selected="selected" disabled="disabled">Distancia (500 mts)</option>
-        <option value="0.310686">500 Metros</option>
-        <option value="0.621371">1 Kilometro</option>
-        <option value="3.10686">5 Kilometros</option>
-        <option value="6.21371">10 Kilometros</option>
-      </select> -->
+      <div class="col-md-12">
 
-      <select class="selectpicker" id="" onchange="location = this.value;">
+        <select class="selectpicker" id="" onchange="location = this.value;">
 
-        <?php if(isset($_GET['range']) AND !empty($_GET['range'])): ?>
-          <?php if($_GET['range'] == 0.3): ?>
-            <option selected="selected" disabled="disabled">Distancia (500 mts)</option>
-          <?php elseif($_GET['range'] == 0.6): ?>
-            <option selected="selected" disabled="disabled">Distancia (1 km)</option>
-          <?php elseif($_GET['range'] == 3.1): ?>
-            <option selected="selected" disabled="disabled">Distancia (5 kms)</option>
-          <?php elseif($_GET['range'] == 6.2): ?>
-            <option selected="selected" disabled="disabled">Distancia (10 kms)</option>
-          <?php elseif($_GET['range'] >= 6.3): ?>
-            <option selected="selected" disabled="disabled">Distancia (<?=$_GET['range']?> ml)</option>
+          <?php if(isset($_GET['range']) AND !empty($_GET['range'])): ?>
+            <?php if($_GET['range'] == 0.3): ?>
+              <option selected="selected" disabled="disabled">Distancia (500 mts)</option>
+            <?php elseif($_GET['range'] == 0.6): ?>
+              <option selected="selected" disabled="disabled">Distancia (1 km)</option>
+            <?php elseif($_GET['range'] == 3.1): ?>
+              <option selected="selected" disabled="disabled">Distancia (5 kms)</option>
+            <?php elseif($_GET['range'] == 6.2): ?>
+              <option selected="selected" disabled="disabled">Distancia (10 kms)</option>
+            <?php elseif($_GET['range'] >= 6.3): ?>
+              <option selected="selected" disabled="disabled">Distancia (<?=$_GET['range']?> ml)</option>
+            <?php else: ?>
+              <option selected="selected" disabled="disabled">Distancia (<?=$_GET['range']?> ml)</option>
+            <?php endif; ?>
           <?php else: ?>
-            <option selected="selected" disabled="disabled">Distancia (<?=$_GET['range']?> ml)</option>
+            <option selected="selected" disabled="disabled">Distancia (500 mts)</option>
           <?php endif; ?>
-        <?php else: ?>
-          <option selected="selected" disabled="disabled">Distancia (500 mts)</option>
-        <?php endif; ?>
 
-        <option value="?range=0.3">500 Metros</option>
-        <option value="?range=0.6">1 Kilometro</option>
-        <option value="?range=3.1">5 Kilometros</option>
-        <option value="?range=6.2">10 Kilometros</option>
-      </select>
+          <option value="?range=0.3">500 Metros</option>
+          <option value="?range=0.6">1 Kilometro</option>
+          <option value="?range=3.1">5 Kilometros</option>
+          <option value="?range=6.2">10 Kilometros</option>
+        </select>
 
-      <!-- <select class="selectpicker" id="">
-        <option value="allCat" selected="selected" disabled="disabled">Categorias</option>
-        <option value="all">Todas</option>
-        <option>Comidas</option>
-        <option>Bancos</option>
-        <option>Salud</option>
-        <option>Ferreterias</option>
-      </select> -->
+        <button class="btn btn-warning" href="#!" id="findMe">¡CERCA A MI!</button>
 
-      <button class="btn btn-warning" href="#!" id="findMe">¡CERCA A MI!</button>
-
-      <div class="clearfix"></div>
+      </div>
 
     </div>
-
   </div>
 
-  <div id="map"></div>
   
+
   <script type="text/javascript" src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="bower_components/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
   <script type="text/javascript">
